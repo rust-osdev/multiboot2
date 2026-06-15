f29b2374c0344f766c3ea0ef79edd1d6adb40a80
Philipp Schuster <phip1611@gmail.com>
2026-06-15 08:41:38 +0200

    flake: update

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell rec {
   packages = with pkgs; [
     # general
     rustup
-    nixpkgs-fmt
-    niv
 
     # integration test
     grub2
@@ -13,7 +13,7 @@ pkgs.mkShell rec {
     xorriso
 
     (pkgs.writeShellScriptBin "run-integrationtest" ''
-    ./integration-test/run.sh
+      ./integration-test/run.sh
     '')
   ];
 }
